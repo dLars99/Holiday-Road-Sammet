ccf87d1e8842162a79c6117d0c7be9d3978060db
David Larsen <davidplarsen@gmail.com>
2020-06-10 12:01:27 -0500

    Begin detail window construction

--- scripts/parks/ParkItinerary.js
+++ scripts/parks/ParkItinerary.js
@@ -2,5 +2,7 @@ const addParkToItinerary = (selectedPark) => {
     return `<img class="itineraryPark__Image" src="${selectedPark.images[0].url}" 
     alt="${selectedPark.images[0].altText}">
     <h3 class="itineraryPark__Name">${selectedPark.fullName}</h3>
-     <button class="park__button">Details</button>`
+    <div class="popup__button" onclick="parkDetails()">Park Details
+        <span class="popup__window" id="parkDetails">Insert details here</span>
+    </div>
 }

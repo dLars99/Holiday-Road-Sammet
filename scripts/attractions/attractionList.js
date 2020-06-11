e47f910b09b558065bae8019cc08cc7d9081343b
David Larsen <davidplarsen@gmail.com>
2020-06-11 11:12:43 -0500

    Update save functionality with new master data

--- scripts/attractions/attractionList.js
+++ scripts/attractions/attractionList.js
@@ -19,10 +19,9 @@ const attractionDropdown = document.querySelector(".attractionLog")
 //Listens for changes to the dropdown and activates the next bit
 attractionDropdown.addEventListener("change", clickEvent => {
 
-    tripItinerary.attraction = clickEvent.target.value
+    tripItinerary.attraction = parseInt(clickEvent.target.value)
 
-    itineraryAttractionObject = tempCollection.find( place  => place.id.toString() === tripItinerary.attraction)
-
+    itineraryAttractionObject = tempCollection.find( place  => place.id === tripItinerary.attraction)
 
     const attractionSelectionHTML = addAttractionToItinerary(itineraryAttractionObject)
     document.querySelector(".attraction").innerHTML = attractionSelectionHTML
--- scripts/itineraries/save.js
+++ scripts/itineraries/save.js
@@ -8,23 +8,36 @@ const checkItineraryFullness = () => {
 
 
 const saveCurrentItinerary = () => {
-    // Convert IDs to integers
-    tripItinerary.attraction = parseInt(tripItinerary.attraction)
-    tripItinerary.eatery = parseInt(tripItinerary.eatery)
-    console.log(tripItinerary)
+    // console.log(tripItinerary)
     itineraryJSON = JSON.stringify(tripItinerary)
-    console.log(itineraryJSON)
-    // 1. Create a new XMLHttpRequest object
-    let xhr = new XMLHttpRequest();
+    // console.log(itineraryJSON)
+    // // 1. Create a new XMLHttpRequest object
+    // let xhr = new XMLHttpRequest()
 
-    // 2. Configure it: GET-request for the URL /article/.../load
-    xhr.open('POST', 'http://localhost:8088/itineraries', true);
-    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    // // 2. Configure it: GET-request for the URL /article/.../load
+    // xhr.open('POST', 'http://localhost:8088/itineraries', true)
+    // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8')
 
-    // 3. Send the request over the network
-    xhr.send(itineraryJSON);
+    // // 3. Send the request over the network
+    // xhr.send(itineraryJSON)
+    //     .then(() => refreshPage())
 
     // Reload the page
-    location.reload()
+    // location.reload()
+
+    fetch("http://localhost:8088/itineraries", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: itineraryJSON
+    }).then(() => {
+        // Refresh the page after save is complete
+        location.reload()
+    })
 
 }
+
+// const refreshPage = () => {
+//     location.reload()
+// }

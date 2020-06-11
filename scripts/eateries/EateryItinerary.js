41bd9c2151791ecf453304502123490606ab932e
nickglovdev <nickglovdev@gmail.com>
2020-06-11 13:09:21 -0500

    Adding a model. Testing it at the moment.

--- scripts/eateries/EateryItinerary.js
+++ scripts/eateries/EateryItinerary.js
@@ -1,11 +1,16 @@
 const eateryitineraryHTML = (eateryItineraryObject) => {
     return `<div>${eateryItineraryObject.businessName}</div>
-    <button id="myBtn">Open Modal</button>
+    
+<button id="myBtn">Open Modal</button>
 
-        <div id="myModal" class="modal">
-            <div class="modal-content">
-                <span class="close">&times;</span>
-                <p>Some text in the Modal..</p>
-            </div>
-        </div>`
+
+<div id="myModal" class="modal">
+
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+        `
 }
--- scripts/eateries/EateryList.js
+++ scripts/eateries/EateryList.js
@@ -9,7 +9,7 @@ const eateryList = () => {
 }
 
 // Object to be used to hold the current selection of the section options.
- let eateryObject = {}
+let eateryObject = {}
 
 // Create a document selector for our addVentListener.
 const eaterySelection = document.querySelector(".eateryList")
@@ -21,8 +21,33 @@ eaterySelection.addEventListener("change", (clickEvent) => {
     tripItinerary.eatery = parseInt(clickEvent.target.value)
     // Using find to find the id. Found out that the id 
     eateryObject = eateryCollection.find(eatery => eatery.id === tripItinerary.eatery)
-    
+
     const selectHTMLEatery = eateryitineraryHTML(eateryObject)
     document.querySelector(".eatery").innerHTML = selectHTMLEatery
-    
+
+    const modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    const btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
 })

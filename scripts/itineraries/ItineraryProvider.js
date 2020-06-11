5c50decb9e8cf2261607bf01ce6549038c3dee67
David Larsen <davidplarsen@gmail.com>
2020-06-11 16:13:08 -0500

    Correct itinerary url variable name

--- scripts/itineraries/ItineraryProvider.js
+++ scripts/itineraries/ItineraryProvider.js
@@ -1,6 +1,6 @@
 // Retrieve itinerary data from local JSON server
 const getItineraryData = () => {
-    const parksURL = `http://localhost:8088/itineraries`
-    return fetch(parksURL)
+    const itineraryURL = `http://localhost:8088/itineraries`
+    return fetch(itineraryURL)
         .then(response => response.json())
 }

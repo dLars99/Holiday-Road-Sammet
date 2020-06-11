0c8f1ded07b4059bff41be7d282eef5663e7140d
nickglovdev <nickglovdev@gmail.com>
2020-06-11 16:26:08 -0500

    Remove an with my select option

--- scripts/eateries/Eatery.js
+++ scripts/eateries/Eatery.js
@@ -2,7 +2,6 @@ const eateryConverter = (eateryObject) => {
 
     const eateryHTMLRepresentation = 
         `
-        <option disable>Select Eatery</option>
         <option value="${eateryObject.id}">${eateryObject.businessName}</option>
         `
     return eateryHTMLRepresentation

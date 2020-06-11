7291c5b5ac1852f2570d890c041a433e869c9910
nickglovdev <nickglovdev@gmail.com>
2020-06-11 11:29:57 -0500

    Working on getting a model to work instead of a basic popup. In the middle of trying to figure out how to set up functions.

--- scripts/eateries/EateryItinerary.js
+++ scripts/eateries/EateryItinerary.js
@@ -1,5 +1,11 @@
 const eateryitineraryHTML = (eateryItineraryObject) => {
-    console.log(eateryItineraryObject)
-    return`<div>${eateryItineraryObject.businessName}</div>
-    <button class="detail__button">Details Eatery</button>`
+    return `<div>${eateryItineraryObject.businessName}</div>
+    <button id="myBtn">Open Modal</button>
+
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <p>Some text in the Modal..</p>
+            </div>
+        </div>`
 }

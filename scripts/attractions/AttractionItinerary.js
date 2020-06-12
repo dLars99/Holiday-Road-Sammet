8c8479b7a4da209fc0dcc11c6c8f466508f66a5c
Erik Lindstrom <elindstrom42@gmail.com>
2020-06-12 10:24:59 -0500

    cleaned up

--- scripts/attractions/AttractionItinerary.js
+++ scripts/attractions/AttractionItinerary.js
@@ -1,6 +1,6 @@
 const addAttractionToItinerary = (selectedAttraction) => {
     return `
-<h3 class="itineraryAttraction__Name">${selectedAttraction.name}</h3>
+<h3 class="itineraryActivity__Name">${selectedAttraction.name}</h3>
  <div class="popup__button" onclick="attractionDetails()"><button>Bizarrerie Details</button> 
  <span class="popup__window" id="attractionDetails">
  <h2>${selectedAttraction.name}</h2> 
